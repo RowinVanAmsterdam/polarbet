0e4f8ed6528b331dcc7b409560f73110c9129562
Rowin206 <rowin.amsterdam@gmail.com>
2021-01-04 15:05:29 +0100

    Added gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <div className="Container">
         <Header />
         <Switch>
-          <Route path="/" exact component={Home}></Route>
+          <Route path="/home" exact component={Home}></Route>
         </Switch>
       </div>
     </Router>
