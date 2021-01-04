e054240b7e611fcde2d876d725ef8d41936d0112
Rowin206 <rowin.amsterdam@gmail.com>
2021-01-04 15:58:59 +0100

    build

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="Container">
         <Header />
         <Switch>
